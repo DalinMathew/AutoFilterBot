26b49916f33eccbe0aada4dae866b067586a354e
Ragnar Lothbrock <84623965+DalinMathew@users.noreply.github.com>
2021-05-24 20:11:55 +0530

    Create translation.py

---
+++ Bot/translation.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) @DalinMathew
+
+class Translation(object):
+    
+    START_TEXT = """<b>Hey {} ☺️!!</b>
+<i>Am Just A Advance Auto Filter Bot Created For <b>Malayalam Rockers Group</b>..
+
+Subscribe to our Telegram Channel and Telegram Group.🍒 !!
+
+@MalRok
+</i>"""    
+    
+    HELP_TEXT = """
+<b><i><u>🍒 No Help At All 🍒</u></i></b>
+"""
+    
+    ABOUT_TEXT = """<b>🍒 Name</b> : <code> Auto Filter Bot</code>
+    
+<b>🍒 Creator</b> : <b><i><a href="https://t.me/null_fallaci">Dalin Mathew</a></i></b>
+
+<b>🍒 Language</b> : <code>Python3</code>
+
+<b>🍒 Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
+
+"""
