c611892e6b5a62d1490095ee073f75cfe09f74a3
Ragnar Lothbrock <84623965+DalinMathew@users.noreply.github.com>
2021-05-24 21:10:01 +0530

    Update callback.py

--- Bot/plugins/callback.py
+++ Bot/plugins/callback.py
@@ -52,7 +52,7 @@ async def cb_navg(bot, update: CallbackQuery):
         VERIFY[str(chat_id)] = admin_list
     
     if not ((user_id == ruser_id) or (user_id in admin_list)): # Checks if user is same as requested user or is admin
-        await update.answer("Nice Try ;)",show_alert=True)
+        await update.answer("Only The Requested Person Can Do This ☺️",show_alert=True)
         return
 
 
