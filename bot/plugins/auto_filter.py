dd78296a6264ceaa2f0166394bbfc02488e6c103
Ragnar Lothbrock <84623965+DalinMathew@users.noreply.github.com>
2021-05-25 17:52:17 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -81,7 +81,7 @@ async def auto_filter(bot, update):
             file_size = "" if file_size == ("[0 B]") else file_size
             
             # add emoji down below inside " " if you want..
-            button_text = f"{file_size}-📂{file_name}"
+            button_text = f"{file_size}- 📂{file_name}"
             
 
             if file_type == "video":
@@ -203,7 +203,7 @@ async def auto_filter(bot, update):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                text=f"Check Links at @MalRok.. /nFound {(len_results)} Results For Your Request: <code>{query}</code> 😎",
+                text=f"Check Links at @MalRok..  ⚜️  Found {(len_results)} Results For Your Request: <code>{query}</code> 😎",
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
