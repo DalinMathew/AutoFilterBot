cd96830531a17be227ad8bf9cc0394ae6705676e
Ragnar Lothbrock <84623965+DalinMathew@users.noreply.github.com>
2021-06-06 13:17:37 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -81,7 +81,7 @@ async def auto_filter(bot, update):
             file_size = "" if file_size == ("[0 B]") else file_size
             
             # add emoji down below inside " " if you want..
-            button_text = f"{file_size}- 📂{file_name}"
+            button_text = f"⭐{file_size}{file_name}⭐"
             
 
             if file_type == "video":
