a043a9146a5a6222ba3cbc9fac284f01b5eccd22
Ragnar Lothbrock <84623965+DalinMathew@users.noreply.github.com>
2021-06-06 13:15:47 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -8,7 +8,7 @@ class Translation(object):
 <i>Am Just A Advance Auto Filter Bot Created to Work in <i>Malayalam Rockers Group. 😉.</i>
 You Can't Add Me to Your Group !! 🍒
 
-You can create Your own by Clicking on Deploy Tutorial📜 !!!😎 </b>
+You can create Your own by Clicking on Deploy Tutorial for📜 !!!😎 </b>
 
 @MalRok 
 </i>"""    
