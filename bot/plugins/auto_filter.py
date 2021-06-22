d03e1700d9e2749ba833d2b262c3930933f38f99
Ragnar Lothbrock <84623965+DalinMathew@users.noreply.github.com>
2021-06-22 17:08:12 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -147,7 +147,7 @@ async def auto_filter(bot, update):
         if len_result != 1:
             result[0].append(
                 [
-                    InlineKeyboardButton("🍒 GoTo NextPage 🍒", callback_data=f"navigate(0|next|{query})")
+                    InlineKeyboardButton("➜ GoTo NextPage ➜", callback_data=f"navigate(0|next|{query})")
                 ]
             )
         
