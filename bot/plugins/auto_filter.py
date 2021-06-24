7bc5f389918725bd6abb3d30c628afb14dac68e5
Ragnar Lothbrock <84623965+DalinMathew@users.noreply.github.com>
2021-06-24 12:00:39 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -203,7 +203,7 @@ async def auto_filter(bot, update):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                text=f"⚜️ Check Links at @MalRok..  ⚜️  Found {(len_results)} Results For Your Request: <code>{query}</code> 😎",
+                text=f<img src="https://telegra.ph/file/a5df0b4e83cdcca08b918.jpg">"⚜️ Check Links at @MalRok..  ⚜️  \nFound {(len_results)} Results For Your Request: <code>{query}</code> 😎",
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
